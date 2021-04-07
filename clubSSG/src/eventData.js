7ee5df8680e572ab6e037d6fbf0571bf450b6306
Michael <mlarussa49@yahoo.com>
2021-04-07 13:22:47 -0700

    Removed clubSSG and cleaned README

--- clubSSG/src/eventData.js
+++
@@ -1,6 +0,0 @@
-events = [{"name": "Fallout Fridays",
-           "dates": ["February 19th", "February 26th", "March 5th", "March 12th", "March 19th", "March 26th", "etc..."]},
-         {"name": "Skyrim Saturdays",
-          "dates": ["February 20th", "February 27th", "March 6th", "March 13th", "March 21st", "March 27th", "etc..."]},
-         {"name": "Wabbajack training Wednesdays", 
-		  "dates": ["The first Wednesday of every month."]}];
